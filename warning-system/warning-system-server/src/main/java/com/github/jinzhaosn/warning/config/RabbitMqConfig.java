63a9ebd692a8eba890205ad5cfc76bca2dc9fd68
jinzhaosn <961374431@qq.com>
2022-02-17 23:56:42 +0800

    1.[new] data-pusher数据推送 rabbit mq交流配置

--- warning-system/warning-system-server/src/main/java/com/github/jinzhaosn/warning/config/RabbitMqConfig.java
+++ warning-system/warning-system-server/src/main/java/com/github/jinzhaosn/warning/config/RabbitMqConfig.java
@@ -25,11 +25,8 @@ import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
